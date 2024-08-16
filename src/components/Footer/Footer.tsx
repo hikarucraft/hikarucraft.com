63c40a9a872cc9af897f1960a7a1a0da0ee438e9
hikarucraft <147622480+hikarucraft@users.noreply.github.com>
2024-08-17 03:43:33 +0900

    feat: Prepared the necessary components.

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+import styles from './Footer.module.scss';
+export const Footer = () => {
+  return (
+    <>
+      <div className={styles.footer}>copyright 2024 hikarucraft</div>
+    </>
+  );
+};
